5a3d3f812e7f611815d6d032ea1d32d54bfbb97d
Muhammad Alaraby <mohamedelaraby1995@gmail.com>
2019-08-24 06:16:19 +0200

    Design main Screen

---
+++ Swoosh/Custom Views/BorderButton.swift
@@ -0,0 +1,16 @@
+//
+//  BorderButton.swift
+//  Swoosh
+//
+//  Created by Winston on 8/24/19.
+//  Copyright © 2019 Winston. All rights reserved.
+//
+
+import UIKit
+
+class BorderButton: UIButton {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
