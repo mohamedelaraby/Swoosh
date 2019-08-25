5995fbc0f80e67daf33df38369d8855f6e755d75
Muhammad Alaraby <mohamedelaraby1995@gmail.com>
2019-08-25 08:39:57 +0200

    Auto layout the  welcome screen

--- Swoosh/ViewController.swift
+++ Swoosh/ViewController.swift
@@ -9,10 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    /*---[ @IBOutlets ]----*/
+    @IBOutlet weak var swooshLogo: UIImageView!
+    @IBOutlet weak var backgroundImage: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+     
+    }
+    
+    
+    //MARK:- Auto layout frames programmatically
+    private func autoLayout(){
+        //Auto center swoosh logo
+        swooshLogo.frame = CGRect(x: view.frame.size.width / 2 - swooshLogo.frame.size.width / 2, y: 50, width: swooshLogo.frame.size.width, height:swooshLogo.frame.size.height)
+        
+        //Auto Fill background image
+        backgroundImage.frame = view.frame
+        
     }
 
 
